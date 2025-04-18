5c144e248d03b7e8bcc75feeeadf5c4919ca7263
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-18 21:59:18 +0200

    feat: format element numbers
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/gravyboat/skyblockrpc/Element.kt
+++ src/main/kotlin/tech/gravyboat/skyblockrpc/Element.kt
@@ -3,16 +3,17 @@ package tech.gravyboat.skyblockrpc
 import tech.gravyboat.skyblockrpc.config.Config
 import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
 import tech.thatgravyboat.skyblockapi.api.profile.CurrencyAPI
+import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedString
 
 enum class Element(val example: String, val getter: () -> String) {
     PURSE("Purse: 123,456", {
-        "Purse: ${CurrencyAPI.purse}"
+        "Purse: ${CurrencyAPI.purse.toFormattedString()}"
     }),
     BANK("Bank: 123,456", {
-        "Bank: ${CurrencyAPI.bank}"
+        "Bank: ${CurrencyAPI.bank.toFormattedString()}"
     }),
     BITS("Bits: 123,456", {
-        "Bits: ${CurrencyAPI.bits}"
+        "Bits: ${CurrencyAPI.bits.toFormattedString()}"
     }),
     ISLAND("Island: The End", {
         "Island: ${LocationAPI.island?.toString() ?: "Unknown"}"
