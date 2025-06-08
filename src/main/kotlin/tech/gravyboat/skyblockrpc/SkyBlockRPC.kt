3998616e09036ec8504c216c7578597552bdaa30
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-06-08 10:51:21 +0200

    feat: update checker
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/gravyboat/skyblockrpc/SkyBlockRPC.kt
+++ src/main/kotlin/tech/gravyboat/skyblockrpc/SkyBlockRPC.kt
@@ -2,8 +2,11 @@ package tech.gravyboat.skyblockrpc
 
 import com.teamresourceful.resourcefulconfig.api.client.ResourcefulConfigScreen
 import com.teamresourceful.resourcefulconfig.api.loader.Configurator
-import net.fabricmc.api.ModInitializer
+import me.owdding.lib.utils.MeowddingUpdateChecker
+import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.MutableComponent
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import tech.gravyboat.skyblockrpc.config.Config
@@ -17,21 +20,54 @@ import tech.thatgravyboat.skyblockapi.api.events.misc.RegisterCommandsEvent
 import tech.thatgravyboat.skyblockapi.api.events.profile.ProfileChangeEvent
 import tech.thatgravyboat.skyblockapi.api.events.time.TickEvent
 import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.utils.text.Text
+import tech.thatgravyboat.skyblockapi.utils.text.Text.send
+import tech.thatgravyboat.skyblockapi.utils.text.TextColor
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.hover
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.url
 
-object SkyBlockRPC : ModInitializer, Logger by LoggerFactory.getLogger("SkyBlockRPC") {
+object SkyBlockRPC : ClientModInitializer, Logger by LoggerFactory.getLogger("SkyBlockRPC") {
 
     val SELF = FabricLoader.getInstance().getModContainer("skyblockrpc").get()
     val VERSION: String = SELF.metadata.version.friendlyString
 
+    val prefix = Text.join(
+        Text.of("[").withColor(TextColor.GRAY),
+        Text.of("SbRPC").withColor(TextColor.AQUA),
+        Text.of("] ").withColor(TextColor.GRAY),
+    )
+
     val configurator = Configurator("sbrpc")
 
-    override fun onInitialize() {
+    override fun onInitializeClient() {
         Config.register(configurator)
-
+        MeowddingUpdateChecker("qESHWJ0N", SELF, ::sendUpdateMessage)
         SkyBlockAPI.eventBus.register(this)
 
         RPCClient.start()
     }
+
+    fun sendUpdateMessage(link: String, current: String, new: String) {
+        fun MutableComponent.withLink() = this.apply {
+            this.url = link
+            this.hover = Text.of(link).withColor(TextColor.GRAY)
+        }
+
+        McClient.tell {
+            Text.of().send()
+            Text.join(
+                "New version found! (",
+                Text.of(current).withColor(TextColor.RED),
+                Text.of(" -> ").withColor(TextColor.GRAY),
+                Text.of(new).withColor(TextColor.GREEN),
+                ")",
+            ).withLink().sendWithPrefix()
+            Text.of("Click to download.").withLink().sendWithPrefix()
+            Text.of().send()
+        }
+    }
+
+    fun Component.sendWithPrefix() = Text.join(prefix, this).send()
 
     @Subscription
     fun onRegisterCommands(event: RegisterCommandsEvent) {
