ab61cf668fea76783076d246f7aa621d77ce2cf6
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-03-02 11:09:20 +0100

    drop dead versions
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -8,10 +8,6 @@ stonecutter active "1.21.11"
 stonecutter parameters {
     swaps["mod_version"] = "\"" + property("version") + "\";"
     swaps["minecraft"] = "\"" + node.metadata.version + "\";"
-    replacements.string {
-        direction = eval(current.version, "> 1.21.5")
-        replace("// moj_import <", "//!moj_import <")
-    }
 
     filters.include("**/*.fsh", "**/*.vsh")
 }
