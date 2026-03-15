12c781d3c6c3d88e612ecb6b04f48b63f9839742
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-03-15 14:08:13 +0100

    refactor: elitebot -> eliteskyblock
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/skyblockrpc/Buttons.kt
+++ src/main/kotlin/me/owdding/skyblockrpc/Buttons.kt
@@ -6,7 +6,7 @@ import tech.thatgravyboat.skyblockapi.helpers.McPlayer
 enum class Buttons(val label: String, val url: Lazy<String>) {
     MODRINTH("Modrinth", lazyOf("https://modrinth.com/mod/skyblock-rpc")),
     SKY_CRYPT("SkyCrypt", lazy { "https://sky.shiiyu.moe/stats/${McPlayer.name}" }),
-    ELITE_BOT("EliteBot", lazy { "https://elitebot.dev/@${McPlayer.name}" }),
+    ELITE_BOT("EliteSkyBlock", lazy { "https://eliteskyblock.com/@${McPlayer.name}" }), // TODO: rename enum entry
     ;
 
     fun toButton() = RichPresenceButton(url.value, label)
